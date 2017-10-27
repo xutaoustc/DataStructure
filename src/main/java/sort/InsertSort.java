3c5d0978995a91a4f212d3ef4f95b142cdc1fcf1
xutao <xutao@fruitday.com>
2017-10-27 22:05:27 +0800

    commit

--- src/main/java/sort/InsertSort.java
+++ src/main/java/sort/InsertSort.java
@@ -3,7 +3,7 @@ package sort;
 public class InsertSort {
     public static void sort(int[]array){
         //把一个元素往已经排好序的子数组里不停插入
-        for(int i=1;i<array.length-1;i++){
+        for(int i=1;i<array.length;i++){
             int temp = array[i];
             int j = i -1;
 
@@ -12,7 +12,9 @@ public class InsertSort {
                 j--;
             }
 
+
             array[j+1]=temp;
+
         }
     }
 }
--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,17 +1,47 @@
-import sort.BubbleSort;
-import sort.InsertSort;
-import sort.MergeSort;
+import sort.*;
 
 public class Test {
     public static void main(String[]args){
-        int[] number = {1,3,5,7,9,2,4,6,8,10};
+        int LENGTH= 300000;
 
-
-        InsertSort.sort(number);
-
-
-        for(int i =0 ;i<number.length;i++){
-            System.out.println(number[i]);
+        int[] number = new int[LENGTH];
+        for(int i =0;i<LENGTH;i++){
+            number[i] = (int)(Math.random() * LENGTH);
         }
+
+        long begintime,endtime=0;
+        int[] a = null;
+
+//        a = number.clone();
+//        begintime = System.currentTimeMillis();
+//        InsertSort.sort(number);
+//        endtime=System.currentTimeMillis();
+//        System.out.println("Insert sort" + (endtime-begintime));
+
+        a = number.clone();
+        begintime = System.currentTimeMillis();
+        InsertOptimizeSort.sort(number);
+        endtime=System.currentTimeMillis();
+        System.out.println("InsertOptimize sort" + (endtime-begintime));
+
+//        a = number.clone();
+//        begintime = System.currentTimeMillis();
+//        BubbleSort.sort(number);
+//        endtime=System.currentTimeMillis();
+//        System.out.println("Bubble sort" + (endtime-begintime));
+
+        a = number.clone();
+        begintime = System.currentTimeMillis();
+        MergeSort.sort(number);
+        endtime=System.currentTimeMillis();
+        System.out.println("Merge sort" + (endtime-begintime));
+
+
+
+        //int[] number = {1,3,5,7,9,2,4,6,8,10};
+//        InsertOptimizeSort.sort(number);
+//        for(int i =0 ;i<number.length;i++){
+//            System.out.println(number[i]);
+//        }
     }
 }
